fa850f4e0b818c9029a70f547bd8365656912af3
Luke Bell <lukekingsleybell@gmail.com>
2015-08-05 11:03:24 +0200

    Orthographic camera view and some texture stuff

--- src/main/Frustum.java
+++ src/main/Frustum.java
@@ -1,16 +1,20 @@
 package main;
 
-//asdkjahsdkjhasdjhakjhsd
-
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PShape;
 
 public class Frustum extends PApplet {
 
+	PImage img;	
+	
 	public void setup() {
 
+		img = loadImage("apple.jpg");		
+		
 		size(640, 640, P3D);
-		background(204);
+		ortho(0,width,0,height);
+		makeBackground(img);
 		
 //		int x=width/2;
 //		int y = height/2;
@@ -18,19 +22,21 @@ public class Frustum extends PApplet {
 //		int topWidth = 50;
 //		int h = 20;
 		
-		for(int i = 1; i < 100; i++){
-			for(int j = 1; j < 100; j++){
-				makeFrustum((width/100)*i, (height/100)*j, width/120, width/240, 10);
+		for(int i = 1; i < 10; i++){
+			for(int j = 1; j < 10; j++){						
+				makeFrustum((width/10)*i, (height/10)*j, width/16, width/32, 5);
 			}
-		}
-		
-		//makeFrustum(x,y,baseWidth,topWidth,h);
+		}			
 		
 	}
 
 	public void makeFrustum(int x, int y, int baseWidth, int topWidth,
-			int h) {
-		beginShape();
+			int h) {			
+		
+		noFill();
+				
+		beginShape();				
+		
 		vertex(x - (baseWidth / 2), y - (baseWidth / 2), 0);
 		vertex(x + (baseWidth / 2), y - (baseWidth / 2), 0);
 		vertex(x + (baseWidth / 2), y + (baseWidth / 2), 0);
@@ -54,6 +60,24 @@ public class Frustum extends PApplet {
 		vertex(x - topWidth / 2, y - topWidth / 2, h);
 		
 		endShape();
+	}
+	
+	public void makeBackground(PImage img) {
+		
+		int imgWidth = img.width;
+		int imgHeight = img.height;
+		
+		beginShape();
+		texture(img);
+		
+		vertex(0,0,0,0,0);
+		vertex(width,0,0,imgWidth,0);
+		vertex(width,height,0,imgWidth,imgHeight);
+		vertex(0,height,0,0,imgHeight);
+		
+		endShape();
+		
+		
 	}
 
 }
