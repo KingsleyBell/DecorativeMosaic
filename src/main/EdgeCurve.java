12e54eb8e4b7438d7101b63723862e3c12e7462b
Josh Buchalter <bchjos003@myuct.ac.za>
2015-08-11 12:37:25 +0200

    Added commenting to edgeCurve class

--- src/main/EdgeCurve.java
+++ src/main/EdgeCurve.java
@@ -9,12 +9,21 @@ import java.util.Scanner;
 import processing.core.PVector;
 
 public class EdgeCurve {
-	ArrayList <PVector> points;
+	ArrayList <PVector> points;  //A list of points that lie on the curve
 	
+	/*
+	 * Generic constructor that initiates an empty list
+	 * How to use: call constructor and then add points to list whenever needed
+	 */
 	public EdgeCurve () {
 		points = new ArrayList <>();
 	}
 	
+	/*
+	 * Method to find and return the closest point on this edge curve from a given point (centroid/mouse click)
+	 * @args: PVector p => location of centroid/mouse click
+	 * @return: String => (pos of closest point in list)-(distance from closest point)
+	 */
 	public String getClosestPoint (PVector P) {
 		double distance = PVector.dist(points.get(0), P);
 		int posOfClosestPoint = 0;
@@ -29,18 +38,35 @@ public class EdgeCurve {
 		return posOfClosestPoint + "-" + distance;
 	}
 	
-	public void addPoint (PVector e) {
-		points.add(e);
+	/*
+	 * Method to add a point to the list
+	 * @args: PVector p => location of point to be added
+	 */
+	public void addPoint (PVector p) {
+		points.add(p);
 	}
 	
+	/*
+	 * Method to return the size of the list of points on the edge curve
+	 * @return int size => size of list / number of points making up curve
+	 */
 	public int getSize() {
 		return points.size();
 	}
 	
+	/*
+	 * Method to return the vector at the given index in list
+	 * @args: int index => position in list wanted
+	 * @return: PVector v => vector at the index's position in list
+	 */
 	public PVector getVector(int index) {
 		return points.get(index);
 	}
 	
+	/*
+	 * Method to store all the points in the list in a textfile
+	 * TODO: find naming convention for textfile location
+	 */
 	public void store() {
 		PrintWriter outputStream = null;
 		try {
@@ -54,14 +80,27 @@ public class EdgeCurve {
 		}
 	}
 	
+	/*
+	 * Method to tell if a given point is on the edge curve i.e. in the list of points
+	 * @args: PVector p => point to check
+	 * @return: boolean b => true if p is on curve
+	 * 					  => false if p is not on curve
+	 */
 	public boolean containsPoint (PVector p) {
 		return points.contains(p);
 	}
 	
+	/*
+	 * Method to load a set of previously stored points from the given textfile
+	 * @args: String fileLoc => location of textfile
+	 */
 	public void loadEdgeCurve (String fileLoc) {
 		Scanner inputStream = null;
 		try {
 			inputStream = new Scanner(new FileInputStream(fileLoc));
+			
+			//for each line: cut off end braces, split the elements into an array and 
+			//convert the string into float components to create the vector of the point
 			while(inputStream.hasNextLine()) {
 				String line = inputStream.nextLine();
 				line = line.substring(2, line.length() - 2);
@@ -71,7 +110,6 @@ public class EdgeCurve {
 					coords[i] = Float.parseFloat(elements[i]);
 				}
 				points.add(new PVector(coords[0], coords[1]));
-//				points.add(e)
 			}
 			inputStream.close();
 		} catch (Exception e) {
@@ -79,6 +117,9 @@ public class EdgeCurve {
 		}
 	}
 	
+	/*
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
 		String returnString = "";
 		for (PVector twoDVector : points) {
