2a0be3f5b7f5922e98e6d923a502ddd51ece73bd
Luke Bell <lukekingsleybell@gmail.com>
2015-09-18 17:45:16 +0200

    JUnit tests

--- src/tests/EdgeCurveTest.java
+++ src/tests/EdgeCurveTest.java
@@ -1,16 +1,22 @@
 package tests;
 
+import static org.junit.Assert.*;
 import main.EdgeCurve;
 
 import org.junit.Test;
 
+import processing.core.PVector;
+
 public class EdgeCurveTest {
 
-	EdgeCurve edgeCureve = new EdgeCurve();
+	EdgeCurve edgeCurve = new EdgeCurve();
 	
 	@Test
-	public void addPointsTest() {
-		
+	public void testAddPoints() {
+		System.out.println("Inside testAddPoints()");
+		PVector p = new PVector(1,1);
+		edgeCurve.addPoint(p);
+		assertEquals(edgeCurve.getVector(0),p);
 	}
 	
 }
---
+++ src/tests/TestSuite.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+   EdgeCurveTest.class
+})
+public class TestSuite {
+}
